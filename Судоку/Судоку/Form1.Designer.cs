2d7f649f5ba293f8272fc2a6fc5088b2e9c8c3b4
Gassyr2001 <75167408+Gassyr2001@users.noreply.github.com>
2025-01-21 16:21:42 +0500

    Add files via upload

---
+++ Судоку/Судоку/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Судоку
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 9;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(30, 30);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(360, 360);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 475);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 475);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Проверить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(28, 406);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(90, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Сложный(60)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(28, 429);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Средний(40)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(28, 452);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(80, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Легкий(20)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 539);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(437, 578);
+            this.MinimumSize = new System.Drawing.Size(437, 578);
+            this.Name = "Form1";
+            this.Text = "Судоку";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Судоку/Судоку/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Судоку
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSudokuGrid();
+            this.tableLayoutPanel1.Paint += new PaintEventHandler(this.panel1_Paint);
+        }
+        private Random rand = new Random();
+        public class SudokuGenerator
+        {
+            private int[,] board = new int[9, 9];
+            private Random rand = new Random();
+
+            // Генерация пустого поля Судоку
+            public int[,] GenerateBoard()
+            {
+                // Заполняем поле по алгоритму
+                FillDiagonal();
+                FillRemaining(0, 3);
+
+                // Удаляем элементы для создания задачи
+                //RemoveNumbers(1); // Удалим 40 чисел для задачи
+
+                return board;
+            }
+
+            // Заполнение диагональных блоков
+            private void FillDiagonal()
+            {
+                for (int i = 0; i < 9; i += 3)
+                {
+                    FillBlock(i, i);
+                }
+            }
+
+            // Заполнение подблока
+            private bool FillBlock(int row, int col)
+            {
+                int[] numbers = new int[9];
+                for (int i = 0; i < 9; i++) numbers[i] = i + 1;
+
+                Shuffle(numbers);
+
+                int index = 0;
+                for (int i = row; i < row + 3; i++)
+                {
+                    for (int j = col; j < col + 3; j++)
+                    {
+                        board[i, j] = numbers[index++];
+                    }
+                }
+
+                return true;
+            }
+
+            // Метод для случайного перемешивания массива
+            private void Shuffle(int[] array)
+            {
+                for (int i = array.Length - 1; i > 0; i--)
+                {
+                    int j = rand.Next(i + 1);
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            // Проверка, можно ли вставить число в клетку
+            private bool IsSafe(int row, int col, int num)
+            {
+                return !UsedInRow(row, num) && !UsedInCol(col, num) && !UsedInBlock(row - row % 3, col - col % 3, num);
+            }
+
+            // Проверка на использование числа в строке
+            private bool UsedInRow(int row, int num)
+            {
+                for (int i = 0; i < 9; i++)
+                    if (board[row, i] == num) return true;
+                return false;
+            }
+
+            // Проверка на использование числа в колонке
+            private bool UsedInCol(int col, int num)
+            {
+                for (int i = 0; i < 9; i++)
+                    if (board[i, col] == num) return true;
+                return false;
+            }
+
+            // Проверка на использование числа в блоке 3x3
+            private bool UsedInBlock(int startRow, int startCol, int num)
+            {
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                        if (board[i + startRow, j + startCol] == num) return true;
+                return false;
+            }
+
+            // Заполнение оставшихся клеток
+            private bool FillRemaining(int row, int col)
+            {
+                if (row == 9)
+                {
+                    return true; // Все строки заполнены
+                }
+                if (col == 9)
+                {
+                    return FillRemaining(row + 1, 0); // Переходим к следующей строке
+                }
+
+                if (board[row, col] != 0)
+                {
+                    return FillRemaining(row, col + 1); // Если клетка уже заполнена, переходим к следующей
+                }
+
+                for (int num = 1; num <= 9; num++)
+                {
+                    if (IsSafe(row, col, num))
+                    {
+                        board[row, col] = num;
+                        if (FillRemaining(row, col + 1)) return true;
+                        board[row, col] = 0; // Откат, если не получилось
+                    }
+                }
+                return false;
+            }
+
+        }
+
+        private int slozh = 2,del =40;
+
+        // Удаление чисел для создания задачи
+        private void RemoveNumbers(int count)
+        {
+            int removed = 0;
+            while (removed < count)
+            {
+                int row = rand.Next(9);
+                int col = rand.Next(9);
+
+                if (solutionBoard[row, col] != 0)
+                {
+                    solutionBoard[row, col] = 0;
+                    removed++;
+                }
+            }
+        }
+
+        // Массив для хранения TextBox
+        private TextBox[,] sudokuTextBoxes = new TextBox[9, 9];
+
+        private int[,] solutionBoard = new int[9, 9];
+        private int[,] solutionBoard2 = new int[9, 9];
+
+        // Метод для инициализации сетки из TextBox
+        private void InitializeSudokuGrid()
+        {
+            // Заполняем TextBox в TableLayoutPanel
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    TextBox txtBox = new TextBox();
+                    txtBox.Width = 30;
+                    txtBox.Height = 30;
+                    txtBox.TextAlign = HorizontalAlignment.Center;
+                    txtBox.MaxLength = 1;  // Только одно число в каждой клетке
+                    txtBox.Font = new System.Drawing.Font("Arial", 14);
+                    txtBox.Name = $"txtBox_{i}_{j}";
+                    tableLayoutPanel1.Controls.Add(txtBox, j, i); // Добавляем в таблицу
+                    sudokuTextBoxes[i, j] = txtBox; // Сохраняем в массив
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black, 2);
+            g.DrawLine(pen, 118, 0, 118, 360);
+            g.DrawLine(pen, 238, 0, 238, 360);
+            g.DrawLine(pen, 0, 118, 360, 118);
+            g.DrawLine(pen, 0, 238, 360, 238);
+        }
+
+        // Метод для генерации и отображения Sudoku
+        private void GenerateAndDisplaySudoku()
+        {
+            // Генерация поля Судоку
+            SudokuGenerator generator = new SudokuGenerator();
+            solutionBoard = generator.GenerateBoard(); // Генерация полного поля для решения
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    solutionBoard2[i, j] = solutionBoard[i, j];
+                }
+            }
+
+            RemoveNumbers(del);
+            // Отображаем значения в TextBox
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sudokuTextBoxes[i, j].BackColor = System.Drawing.Color.White;
+                    sudokuTextBoxes[i, j].Text = solutionBoard[i, j] == 0 ? "" : solutionBoard[i, j].ToString();
+                    if (solutionBoard[i, j] == 0)
+                    {
+                        sudokuTextBoxes[i, j].Enabled = true;
+                    }
+                    else
+                    {
+                        sudokuTextBoxes[i, j].Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Генерируем и отображаем Судоку при загрузке формы
+            GenerateAndDisplaySudoku();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateAndDisplaySudoku();
+        }
+        // Метод для проверки решения
+        private void CheckSolution()
+        {
+            bool isCorrect = true;
+
+            // Проверяем каждое поле
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // Получаем введенное значение из TextBox
+                    string input = sudokuTextBoxes[i, j].Text;
+
+                    // Преобразуем введенное значение в число
+                    int userValue;
+                    if (int.TryParse(input, out userValue) && userValue >= 1 && userValue <= 9)
+                    {
+                        // Сравниваем введенное значение с правильным решением
+                        if (userValue != solutionBoard2[i, j])
+                        {
+                            isCorrect = false;
+                            sudokuTextBoxes[i, j].BackColor = System.Drawing.Color.Red; // Подсвечиваем неправильное поле
+                        }
+                        else
+                        {
+                            sudokuTextBoxes[i, j].BackColor = System.Drawing.Color.White; // Если правильно, сбрасываем цвет
+                        }
+                    }
+                    else
+                    {
+                        // Если введено некорректное значение (не число или число вне диапазона)
+                        isCorrect = false;
+                        // Если поле пустое, пропускаем его
+                        if (string.IsNullOrEmpty(input))
+                        {
+                            sudokuTextBoxes[i, j].BackColor = System.Drawing.Color.Yellow;
+                        }
+                        else
+                        {
+                            sudokuTextBoxes[i, j].BackColor = System.Drawing.Color.Red; // Подсвечиваем неправильное поле
+                        }
+                    }
+                }
+            }
+
+            // Выводим сообщение в зависимости от результата
+            if (isCorrect)
+            {
+                MessageBox.Show("Поздравляем! Решение правильное.", "Проверка решения", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Некоторые числа введены неверно. Проверьте их.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CheckSolution();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            slozh = 3;
+            del = 60;
+            GenerateAndDisplaySudoku();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            slozh = 2;
+            del = 40;
+            GenerateAndDisplaySudoku();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            slozh = 1;
+            del = 20;
+            GenerateAndDisplaySudoku();
+        }
+    }
+}
